d7d4d43a05a1956971a97a90c680a7229d2dd2b7
Jeff Cope <jeff.cope@gmail.com>
2026-02-04 12:55:28 -0700

    first commit

---
+++ src/test/java/com/braindribbler/games/BraindribblingGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braindribbler.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraindribblingGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
